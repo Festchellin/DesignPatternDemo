4d1f562efbf7af74128abbe9b3a0f7179fcc3e78
festchellin <dreamerhaozhe@outlook.com>
2018-03-30 09:30:55 +0800

    add

---
+++ src/com/SingletonPatternDemo/SingletonClient.java
@@ -0,0 +1,13 @@
+package com.SingletonPatternDemo;
+
+import com.SingletonPatternDemo.bean.Singleton;
+
+public class SingletonClient {
+    public static void main(String[] args) {
+        Singleton singleton1 = Singleton.getInstance();
+        Singleton singleton2 = Singleton.getInstance();
+        System.out.println(singleton1 == singleton2);
+        System.out.println("Singleton1:" + singleton1);
+        System.out.println("Singleton2:" + singleton2);
+    }
+}
